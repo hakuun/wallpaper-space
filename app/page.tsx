6e5fef5d7da7e6ad65587df0b76ade47ae8decd9
hakuun <a888888hhb@outlook.com>
2023-08-14 10:34:58 +0800

    add google analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
+import Script from "next/script";
+
 import Gallery from "@/components/gallery";
 import { Navbar } from "@/components/navbar";
 
 export default async function HomePage() {
-  return (
-    <main className="bg-inherit">
-      <Navbar />
-      <div className="p-4 pt-[80px]">
-        <Gallery />
-      </div>
-    </main>
-  );
+	return (
+		<main className="bg-inherit">
+			<Script src="https://www.googletagmanager.com/gtag/js?id=G-H275ER5KMB" />
+			<Script id="google-analytics">
+				{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-H275ER5KMB');
+        `}
+			</Script>
+			<Navbar />
+			<div className="p-4 pt-[80px]">
+				<Gallery />
+			</div>
+		</main>
+	);
 }
